bc893e1c671a4d44b017b56b9f80d587503f3947
Mohamed <mohamedissaoui1124@gmail.com>
2023-09-28 09:00:59 -0700

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
-
-# Define the original sentence with a meaningful variable name
-original_sentence = (
-    "Python is an interpreted, interactive, object-oriented programming "
-    "language that combines remarkable power with very clear syntax"
-)
-
-# Extract and concatenate the desired parts of the sentence
-substring1 = original_sentence[39:67]
-substring2 = original_sentence[107:112]
-substring3 = original_sentence[:6]
-
-result = f"{substring1}{substring2}{substring3}"
-
-# Print the resulting string
-print(result)
+str = "Python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
